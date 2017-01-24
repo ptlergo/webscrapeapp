60a6b7cb518880d33696bf9483c1aed36f8153fb
ptlergo <ptlergo@gmail.com>
2017-01-24 02:59:02 -0500

    xray api init

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const Xray = require('x-ray');
+const xray = Xray();
+
+// URL, targeted selector, object literal structure
+const reqUrl = 'https://soundcloud.com/joey-bada-official/land-of-the-free';
+const response = xray(reqUrl);
+console.log(response);
