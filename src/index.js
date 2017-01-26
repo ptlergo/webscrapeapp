cbf92124546f95b68dd31614c21b3b84e4994d64
ptlergo <ptlergo@gmail.com>
2017-01-25 23:04:33 -0500

    code comments

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const Xray = require('x-ray');
 const x = new Xray();
 console.log('xray run');
 
+/*
+* x(url, scope, array of wrapped objects)
+* wrapping in [] tells xray to iterate once for each scope(.g). without [] itll
+* iterate only once.
+* .g is google search result link
+*/
 x("https://www.google.ca/search?site=&source=hp&q=spice+and+wolf&oq=spice+and+wolf", '.g', [{
    link: 'a@href',
  }]).paginate('#pnnext@href').limit(5)
@@ -11,9 +17,11 @@ x("https://www.google.ca/search?site=&source=hp&q=spice+and+wolf&oq=spice+and+wo
    }
    else {
      obj.forEach((item) => {
+      //  console.log(obj)
        var start = item.link.indexOf('q=')
        var end = item.link.indexOf('&sa')
        item.link = item.link.substring(start + 2, end)
+       // items are links from search result
        console.log(item);
      })
      callback(null, obj)
