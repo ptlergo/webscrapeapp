9e1f700d68810534bee0be62c3e27acf451c8b3d
ptlergo <ptlergo@gmail.com>
2017-02-08 18:51:59 -0500

    image scraped via using img@src

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ app.get('/b', (req, res) => {
   const stream = xray(reqUrl[1], '.oi_post', [{
     title: 'h5',
     excerpt: '.oi_post_descr_preview',
-    thumbnail: '.oi_post_meta_data_holder img'
+    thumbnail: '.oi_post_meta_data_holder img@src'
 
   }]).paginate('a.next.page-numbers a@href').limit(3)
   .stream();
