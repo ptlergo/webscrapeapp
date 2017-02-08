3539f65506ad1619df8710f813abc3f0db12a132
ptlergo <ptlergo@gmail.com>
2017-02-08 16:45:06 -0500

    attempting to grab header of content on 8pounds.com using different selector

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ console.log('xray run');
 app = express();
 
 // URL, targeted selector, object literal structure
-const reqUrl = ['https://blog.ycombinator.com/', '8pounds.com'];
+const reqUrl = ['http://blog.ycombinator.com/', 'http://8pounds.com'];
 const reqSelect = '.post';
 
 app.get('/a', (req, res) => {
@@ -21,12 +21,12 @@ app.get('/a', (req, res) => {
 });
 
 app.get('/b', (req, res) => {
-  const stream = xray(reqUrl[1], reqSelect, [{
-    title: 'h1 a',
-    link: '.article-title@href',
-    arthur: 'span a',
-    excerpt: 'p',
-    thumbnail: 'div iframe'
+  const stream = xray(reqUrl[1], '#posts_holder', [{
+    title: '',
+    link: '',
+    arthur: '',
+    excerpt: '',
+    thumbnail: ''
   }]).paginate('.nav-previous a@href').limit(3)
   .stream();
   stream.pipe(res);
@@ -35,3 +35,5 @@ app.get('/b', (req, res) => {
 const server = app.listen(3000, () => {
   console.log('listening to 3000');
 });
+
+// .article-title@href
