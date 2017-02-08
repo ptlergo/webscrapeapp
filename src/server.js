6a6a0e479b38d9266bc96a6bd4c5ffb322f4ed60
ptlergo <ptlergo@gmail.com>
2017-02-08 16:20:06 -0500

    testing with string const and pipeing result to a view. SUCCESS

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const Xray = require('x-ray');
 const xray = Xray();
+console.log('xray run');
+app = express();
 
 // URL, targeted selector, object literal structure
-const reqUrl = 'http://www.thefader.com/';
-const reqSelect = 'body@html';
+const reqUrl = 'https://blog.ycombinator.com/';
+const reqSelect = '.post';
 
-const result = xray(reqUrl, reqSelect)(function(err, reqSelect){
-  if(reqSelect != null)
-    console.log(reqSelect);
-  else
-    console.log('error! ' + err);
-}).write('results.json');
+app.get('/', (req, res) => {
+  const stream = xray(reqUrl, reqSelect, [{
+    title: 'h1 a',
+    link: '.article-title@href',
+  }]).stream();
+  stream.pipe(res);
+});
+
+const server = app.listen(3000, () => {
+  console.log('listening to 3000');
+});
